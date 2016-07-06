c4b08d6c0e902f234213817e00449963c9a9394f
wangkaili0122 <1783164269@qq.com>
2016-07-06 17:17:31 +0800

    布局文件

--- app/src/main/java/com/example/guju/fragment/BaseFragment.java
+++ app/src/main/java/com/example/guju/fragment/BaseFragment.java
@@ -1,6 +1,7 @@
 package com.example.guju.fragment;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.example.guju.R;
+import com.example.guju.ui.FreedesignActivity;
 
 /**
  * Created by green on 2016/7/4.
@@ -23,6 +25,28 @@ public class BaseFragment extends Fragment {
         View view=inflater.inflate(R.layout.fragment_base,null);
         login = ((ImageView) view.findViewById(R.id.image_login_id));
         freeDesign = ((ImageView) view.findViewById(R.id.image_freedesign_id));
+        //   aboutLogin();
+        aboutFreedesign();
         return view;
     }
+
+    private void aboutFreedesign() {
+        freeDesign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getActivity().getApplicationContext(), FreedesignActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void aboutLogin() {
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
 }
