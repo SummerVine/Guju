b685f8a93bf6214fa6a073342d35bdf8e842d3a5
wangkaili0122 <1783164269@qq.com>
2016-07-07 10:53:42 +0800

    适配器和实体类

--- app/src/main/java/com/example/guju/adapter/StrategyAdapter.java
+++ app/src/main/java/com/example/guju/adapter/StrategyAdapter.java
@@ -1,25 +1,30 @@
 package com.example.guju.adapter;
 
-import android.view.View;
-
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.example.guju.R;
 import com.example.guju.datas.DataServer;
 import com.example.guju.entity.Strategy;
 
-import java.util.List;
+
 
 /**
  * Created by Administrator on 2016/7/6.
  */
 public class StrategyAdapter extends BaseQuickAdapter<Strategy> {
+
+
+    public StrategyAdapter(int dataSize) {
+        super(R.layout.activity_decorateplan_rv_item, DataServer.getStrategy(100));
+    }
+
+
     public StrategyAdapter() {
-        super(R.layout.zhuangxiu_item,null);
+        super( R.layout.zhuangxiu_item, DataServer.getStrategy(100));
     }
 
     @Override
     protected void convert(BaseViewHolder baseViewHolder, Strategy strategy) {
-        baseViewHolder.setText(R.id.tv_explain_id, strategy.getCategoryName());
+        baseViewHolder.setText(R.id.tv_id, strategy.getTypeName());
     }
 }
