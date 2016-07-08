a43ebda85fc645043d324db1bb3575dbbad3fec2
wuliuwei <2419179061@qq.com>
2016-07-08 11:12:14 +0800

    详情界面的四个布局

---
+++ app/src/main/java/com/example/guju/fragment/anliFrament.java
@@ -0,0 +1,8 @@
+package com.example.guju.fragment;
+
+/**
+ * Created by Administrator on 2016/7/8.
+ */
+public class anliFrament extends BaseFragment {
+
+}
---
+++ app/src/main/java/com/example/guju/fragment/lingganFrament.java
@@ -0,0 +1,7 @@
+package com.example.guju.fragment;
+
+/**
+ * Created by Administrator on 2016/7/8.
+ */
+public class lingganFrament extends BaseFragment {
+}
