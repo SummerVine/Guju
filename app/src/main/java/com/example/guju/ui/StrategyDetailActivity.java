b9b8dbe2e31c94383cdd0e0ed6694e9420c913e9
wuliuwei <2419179061@qq.com>
2016-07-08 10:39:15 +0800

    详情界面

--- app/src/main/java/com/example/guju/ui/StrategyDetailActivity.java
+++ app/src/main/java/com/example/guju/ui/StrategyDetailActivity.java
@@ -1,12 +1,13 @@
 package com.example.guju.ui;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 
 import com.example.guju.R;
 
-public class StrategyDetailActivity extends AppCompatActivity {
+public class StrategyDetailActivity  extends Activity {
+    private String[] titles;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
