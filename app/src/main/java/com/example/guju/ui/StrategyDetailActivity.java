d85b06f4f690b8485dc0a2e7747fa84de05bed55
anmengzhen <15537778637@163.com>
2016-07-09 17:24:40 +0800

    第四个页面的框架布局

--- app/src/main/java/com/example/guju/ui/StrategyDetailActivity.java
+++ app/src/main/java/com/example/guju/ui/StrategyDetailActivity.java
@@ -1,71 +1,79 @@
 package com.example.guju.ui;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.guju.R;
-import com.example.guju.fragment.anliFrament;
-import com.example.guju.fragment.fenxingFrament;
-import com.example.guju.fragment.gongluFrament;
-import com.example.guju.fragment.lingganFrament;
+import com.example.guju.bean.Designers;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class StrategyDetailActivity  extends AppCompatActivity {
-    private String[] titles;
-    private List<Fragment> fragments;
-    private ViewPager vp_id;
-    private TabLayout tabs;
+    private CollapsingToolbarLayout collapsing;
+    private ImageView headIcon;
+    private TextView info;
+    private TabLayout tabLayout;
+    private ViewPager content;
+    private List<View> tabs=new ArrayList<>();
+    private String[] subTabs2={"案例","攻略","灵感集","分享"};
+    private List<String> subTabs1=new ArrayList<>();
+    private TextView count;
+    private TextView sort;
+    private String name;
+    private String userImg;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.strategyactivity_main);
-
-        vp_id= (ViewPager) findViewById(R.id.viewpager_id);
-        tabs= (TabLayout) findViewById(R.id.tabs);
-aboutTabLayout();
-        aboutViewPager();
+        setContentView(R.layout.activity_trategy);
+        initData();
+        initView();
+        initTabs();
     }
 
-    private void aboutViewPager() {
-       fragments=new LinkedList<>();
-        Fragment anli=new anliFrament();
-        Fragment gonglu=new gongluFrament();
-        Fragment linggan=new lingganFrament();
-        Fragment fenxing=new fenxingFrament();
-        fragments.add(anli);
-        fragments.add(linggan);
-        fragments.add(gonglu);
-        fragments.add(fenxing);
-        FragmentPagerAdapter adapter=new FragmentPagerAdapter(getSupportFragmentManager()) {
-            @Override
-            public Fragment getItem(int position) {
-                return fragments.get(position);
-            }
+    private void initData() {
+        Intent intent=new Intent();
+        Bundle bundle=intent.getExtras();
+        name = (String) bundle.get("userName");
+        userImg = (String) bundle.get("userImage");
+        subTabs1.add((String) bundle.get("projectNum"));
+        subTabs1.add((String) bundle.get("strategyNum"));
+        subTabs1.add((String) bundle.get("ideabookNum"));
+        subTabs1.add((String) bundle.get("photoNum"));
 
-            @Override
-            public int getCount() {
-                return fragments.size();
-            }
-
-            @Override
-            public CharSequence getPageTitle(int position) {
-                return titles[position];
-            }
-        };
-        vp_id.setAdapter(adapter);
     }
 
-    private void aboutTabLayout() {
-        titles=new String[]{"案例","攻略","灵感集","分享"};
-        tabs.setupWithViewPager(vp_id);
+    private void initTabs() {
+       tabLayout.setTabMode(TabLayout.MODE_FIXED);
+        for (int i = 0; i <4 ; i++) {
+            View view =View.inflate(this,R.layout.item_tabs,null);
+            count = ((TextView) view.findViewById(R.id.count_stategytab_id));
+            sort = ((TextView) view.findViewById(R.id.sort_stategy_id));
+            Designers.ProfessionalsBean dp=new Designers.ProfessionalsBean();
+            count.setText(subTabs1.get(i));
+            sort.setText(subTabs2[i]);
+            tabLayout.addTab(tabLayout.newTab().setCustomView(view));
+        }
+
     }
 
+    private void initView() {
+        collapsing = ((CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar));
+        headIcon = ((ImageView) findViewById(R.id.headicon_stategy_id));
+        info = ((TextView) findViewById(R.id.info_stategy_id));
+        tabLayout = ((TabLayout) findViewById(R.id.tabs_stategy_id));
+        content = ((ViewPager) findViewById(R.id.viewpager_stategy_id));
+        collapsing.setTitle(name);
+        collapsing.setStatusBarScrimColor(Color.rgb(2,68,144));
+    }
 }
