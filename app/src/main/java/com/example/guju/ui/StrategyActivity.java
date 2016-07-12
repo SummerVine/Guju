371a6294f80ced166f6205b3a675ff8f7f00596d
anmengzhen <15537778637@163.com>
2016-07-12 21:23:14 +0800

    fragment

--- app/src/main/java/com/example/guju/ui/StrategyActivity.java
+++ app/src/main/java/com/example/guju/ui/StrategyActivity.java
@@ -11,11 +11,14 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.guju.R;
 
 import com.example.guju.url.Urlan;
 
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 
 
 public class StrategyActivity extends AppCompatActivity{
@@ -23,69 +26,50 @@ public class StrategyActivity extends AppCompatActivity{
 
     private WebView webView;
     private ImageView back;
-    private ImageView share;
-    private static String p;
+   private ImageView share;
     private Intent intent;
     private String pa;
-    /* public static Handler handler=new Handler(){
-        public void handleMessage(Message msg){
-            super.handleMessage(msg);
-
-            p=msg.toString();
-            //  Log.i("info", "------" + data.size());
-        }
-    };*/
-
+   /* private ImageView xin;
+    private ImageView zhan;
+    private TextView free;*/
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.zhuangxiu_detail);
         webView = ((WebView) findViewById(R.id.webview_id));
         back = (ImageView) findViewById(R.id.back_id);
+      /*  xin = (ImageView) findViewById(R.id.iv_xin);
+        zhan = (ImageView) findViewById(R.id.iv_zhan);
+        free=(TextView)findViewById(R.id.tv_free);*/
         share = (ImageView) findViewById(R.id.share_id);
         intent = getIntent();
         pa = intent.getStringExtra("str");
        // Log.i("pa", "------_________________________" + pa);
-
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
             }
         });
+
          loadWeb();
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StrategyActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+       /*  free.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StrategyActivity.this, FreedesignActivity.class);
+                startActivity(intent);
+            }
+        });*/
 
     }
 
-    /*private void showShare() {
-        ShareSDK.initSDK(this);
-        OnekeyShare oks = new OnekeyShare();
-        //关闭sso授权
-        oks.disableSSOWhenAuthorize();
-
-// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
-        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
-        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
-        oks.setTitle(getString(R.string.share));
-        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
-        oks.setTitleUrl("http://sharesdk.cn");
-        // text是分享文本，所有平台都需要这个字段
-        oks.setText("我是分享文本");
-        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
-        // url仅在微信（包括好友和朋友圈）中使用
-        oks.setUrl("http://sharesdk.cn");
-        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
-        oks.setComment("我是测试评论文本");
-        // site是分享此内容的网站名称，仅在QQ空间使用
-        oks.setSite(getString(R.string.app_name));
-        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
-        oks.setSiteUrl("http://sharesdk.cn");
-
-// 启动分享GUI
-        oks.show(this);
-    }
-*/
     private void loadWeb() {
         WebSettings settings=webView.getSettings();
         settings.setJavaScriptEnabled(true);
@@ -94,8 +78,6 @@ public class StrategyActivity extends AppCompatActivity{
         settings.setUseWideViewPort(true);
         settings.setLoadWithOverviewMode(true);
         webView.setWebChromeClient(new WebChromeClient());
-
-
         webView.setWebViewClient(new WebViewClient(){
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -118,7 +100,4 @@ public class StrategyActivity extends AppCompatActivity{
         }
         return super.onKeyDown(keyCode, event);
     }
-
-
-
 }
