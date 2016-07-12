98f12333c158f786565b4242b3b501010a0e1415
anmengzhen <15537778637@163.com>
2016-07-12 21:22:32 +0800

    fragment

--- app/src/main/java/com/example/guju/fragment/BaseFragment.java
+++ app/src/main/java/com/example/guju/fragment/BaseFragment.java
@@ -2,24 +2,11 @@ package com.example.guju.fragment;
 
 
 import android.app.Fragment;
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-
-
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-
-import com.example.guju.R;
-import com.example.guju.ui.FreedesignActivity;
 
 /**
  * Created by green on 2016/7/4.
  */
 public class BaseFragment extends Fragment {
 
-
 }
+
